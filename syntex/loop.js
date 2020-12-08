91e650492957eb40260f40a347c4df7d6bf51f9f
박순영 <psd0217@oozooinc.com>
2020-12-08 14:37:22 +0900

    반복문

---
+++ syntex/loop.js
@@ -0,0 +1,25 @@
+console.log('A');
+console.log('B');
+//console.log('C1');
+//console.log('C2');
+//console.log('C1');
+//console.log('C2');
+//console.log('C1');
+//console.log('C2');
+//console.log('C1');
+//console.log('C2');//반복이 너무 많다..
+//if(boolean){} //조건문
+//while(boolean){}//반복문
+//while(true) //무한루프~!
+//{
+//  console.log('C1');
+//  console.log('C2');
+//}
+var i = 0;
+while(i < 1) //
+{
+  console.log('C1');
+  console.log('C2');
+  i = i + 1;
+}
+console.log('D');
