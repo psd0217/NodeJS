8da989bc5079afc0ceaf956282a9bdc3d5116131
박순영 <psd0217@oozooinc.com>
2020-12-08 13:55:22 +0900

    comparison

---
+++ syntex/comparison.js
@@ -0,0 +1,9 @@
+console.log(1 + 2);//왼족 오른쪽항 합
+console.log(1==1);//두개가 같은가? 같으면 true, 다르면 false
+console.log(1==2);//두개가 같은가? 같으면 true, 다르면 false
+console.log(1>2);
+console.log(1<2);
+console.log(1===1); // true
+console.log(1===2); // false
+name = 1 //대입연산자
+name === 3 //비교연산자
