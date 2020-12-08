540bc5408fdcae045a83e9d4af2b2c61ce65d61a
박순영 <psd0217@oozooinc.com>
2020-12-08 15:44:48 +0900

    함수

---
+++ syntex/function.js
@@ -0,0 +1,33 @@
+//console.log(1);
+//console.log(2);
+//console.log(3);
+f123();
+console.log('A');
+console.log('B');
+console.log('C');
+//console.log(1);
+//console.log(2);
+//console.log(3);
+f123();
+console.log('C');
+console.log('D');
+console.log('E');
+//console.log(1);
+//console.log(2);
+//console.log(3);
+f123();
+console.log('E');
+console.log('F');
+console.log('G');
+//console.log(1);
+//console.log(2);
+//console.log(3);
+f123();
+
+function f123()
+{
+  console.log(1);
+  console.log(2);
+  console.log(3);
+  console.log(4);
+}
