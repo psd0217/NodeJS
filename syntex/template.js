bbbed79ce55df5fce212352cda33ea9c639ce69f
박순영 <psd0217@oozooinc.com>
2020-12-08 13:32:16 +0900

    NodeJS 시작

---
+++ syntex/template.js
@@ -0,0 +1,16 @@
+var name = 'soon'
+var letter = 'Dear ' + name + ' sad \n\
+\
+gsadfa ' + name + ' sdfasdfsadf\nasdfsa ' + name + ' dfasd ' + name + ' fdksjflksljdlkjflskjdlf\njlaskjdfljklsakjdlfjlsjaldfjal;sjkdf;lkjasldjfklkjasdf';
+
+console.log(letter);
+
+var a = 1; //숫자 리터럴
+var b = '1';//문자 리터럴
+
+//템를릿 리터럴
+var letter = `Dear ${name} sad
+gsadfa ${name} sdfasdfsadf
+asdfsa ${name} dfasd ${name} fdksjflksljdlkjflskjdlf
+jlaskjdfljklsakjdlf  ${1+1}  jlsjaldfjal;sjkdf;lkjasldjfklkjasdf`;
+console.log(letter);
---
+++ syntex/variable2.js
@@ -0,0 +1,5 @@
+
+var name = 'soon'
+var letter = 'Dear ' + name + ' sadgsadfa ' + name + ' sdfasdfsadfasdfsa ' + name + ' dfasd ' + name + ' fdksjflksljdlkjflskjdlfjlaskjdfljklsakjdlfjlsjaldfjal;sjkdf;lkjasldjfklkjasdf';
+
+console.log(letter);
