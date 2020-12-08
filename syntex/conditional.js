f439cf308553dee10989b23fc4df299207efdc12
박순영 <psd0217@oozooinc.com>
2020-12-08 14:07:31 +0900

    조건문 1

---
+++ syntex/conditional.js
@@ -0,0 +1,20 @@
+console.log('A');
+console.log('B');
+//if(true)
+//{//실행
+//  console.log('C1');
+//}
+//if(false)
+//{//무시
+//  console.log('C2');
+//}
+
+if(false) //true
+{//실행
+  console.log('C1');
+}
+else
+{//무시
+  console.log('C2');
+}
+console.log('D');
