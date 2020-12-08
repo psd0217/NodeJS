0ba71651955c58792629a9d6700250bd7a745065
박순영 <psd0217@oozooinc.com>
2020-12-08 14:45:23 +0900

    배열

---
+++ syntex/array.js
@@ -0,0 +1,13 @@
+//array 리터러
+var arr = ['A','B','C','D']//  ,1,true] 숫자도 boolean도 가능
+console.log(arr[1]);
+console.log(arr[3]);
+arr[2] = 3;
+
+console.log(arr);
+
+console.log(arr.length);
+
+arr.push('E');
+
+console.log(arr);
