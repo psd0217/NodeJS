18be32bf5a69f57e97122c56bc09c262273ff9fa
박순영 <psd0217@oozooinc.com>
2020-12-08 15:49:57 +0900

    함수의 입력

---
+++ syntex/function2.js
@@ -0,0 +1,11 @@
+console.log( Math.round(1.6)); //객체.함수
+console.log( Math.round(1.4)); //반올림이구나~
+//입력을 해보쟈!
+
+function sum(first, second) // parameter 매게변수!
+{
+  console.log(first + second);
+}
+
+sum(2,4); //합을 하는 함수
+sum(4,7); // argument (인자)
