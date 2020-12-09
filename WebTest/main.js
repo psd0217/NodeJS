29c9d3a7e09a904fdd0f457820415b93611d7960
박순영 <psd0217@oozooinc.com>
2020-12-09 14:19:38 +0900

    파일 수정

--- WebTest/main.js
+++ WebTest/main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url =  require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body)
+function templateHTML(title, list, body, control)
 {
   return `
   <!doctype html>
@@ -15,7 +15,7 @@ function templateHTML(title, list, body)
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${control}
     ${body}
   </body>
   </html>
@@ -49,7 +49,9 @@ var app = http.createServer(function(request,response){
           var title = 'Welcome2';
           var description = 'Hello, Node.js';
           var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          var template = templateHTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`);
             response.writeHead(200);
             response.end(template);
         });
@@ -62,7 +64,9 @@ var app = http.createServer(function(request,response){
           {
             var title = queryData.id;
             var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            var template = templateHTML(title, list,
+              `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
               response.writeHead(200);
               response.end(template);
           });
@@ -86,7 +90,7 @@ var app = http.createServer(function(request,response){
               <input type="submit">
             </p>
           </form>
-          `);
+          `, '');
           response.writeHead(200);
           response.end(template);
       });
