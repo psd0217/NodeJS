23ce4fd665b1f4f18492e19c50465794cb323fe4
박순영 <psd0217@oozooinc.com>
2020-12-09 15:27:10 +0900

    object

---
+++ syntax/object.js
@@ -0,0 +1,9 @@
+var members = ['soon','egoing','hoya']; //array
+console.log(members[1]);
+
+var roles = {
+  'programmer':'soon',
+  'designer':'egoing',
+  'manager' : 'hoya'
+}
+console.log(roles.designer); //object
