bc12b96b547062c211a23f07af4a17b59a61b5f6
박순영 <psd0217@oozooinc.com>
2020-12-09 14:05:50 +0900

    포스트 방식 전송

--- WebTest/main.js
+++ WebTest/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url =  require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body)
 {
@@ -76,7 +77,7 @@ var app = http.createServer(function(request,response){
         var title = 'WEB - create';
         var list = templateList(filelist);
         var template = templateHTML(title, list, `
-          <form action="http://localhost:3000/process_create" method="post">
+          <form action="http://localhost:3000/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -89,6 +90,22 @@ var app = http.createServer(function(request,response){
           response.writeHead(200);
           response.end(template);
       });
+    }
+    else if(pathName === '/create_process')
+    {
+      var body = '';
+      request.on('data',function(data){
+        body = body + data;
+      });
+      request.on('end',function(){
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        console.log(post.title);
+      });
+
+      response.writeHead(200);
+      response.end('success');
     }
     else
     {
