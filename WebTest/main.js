c109e06305b32c27384b4736767c5f302888284f
박순영 <psd0217@oozooinc.com>
2020-12-09 15:15:36 +0900

    파일 삭제

--- WebTest/main.js
+++ WebTest/main.js
@@ -162,6 +162,21 @@ var app = http.createServer(function(request,response){
         });
       });
     }
+    else if(pathName === '/delete_process')
+    {
+      var body = '';
+      request.on('data',function(data){
+        body = body + data;
+      });
+      request.on('end',function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`data/${id}`, function(error){
+          response.writeHead(302, {Location: `/`});
+          response.end();
+        });
+      });
+    }
     else
     {
         response.writeHead(404);
