1de02aae80abf0a24565de82e23e731f75906315
박순영 <psd0217@oozooinc.com>
2020-12-09 15:31:32 +0900

    객체 반복문

--- syntax/object.js
+++ syntax/object.js
@@ -1,9 +1,23 @@
 var members = ['soon','egoing','hoya']; //array
 console.log(members[1]);
+var i = 0;
+while(i < members.length){
+  console.log('array loop : ', members[i]);
+  i = i + 1;
+}
 
 var roles = {
+  //key : value
   'programmer':'soon',
   'designer':'egoing',
   'manager' : 'hoya'
 }
+
+//둘다 된다
 console.log(roles.designer); //object
+console.log(roles['designer']); //object
+
+for(var name in roles)
+{
+  console.log('object key : ', name, ' value : ', roles[name]);
+}
